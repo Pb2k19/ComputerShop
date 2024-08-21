c56f372405c3cec7681d0aa44b6dcff27df2c09c
Patryk Bartołd <p_bartold@ms.wwsi.edu.pl>
2024-08-21 13:29:42 +0200

    Plain text hash implementation

---
+++ ComputerShop/Server/Cryptography/Hash/PlainTextHashService.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ComputerShop.Server.Cryptography.Hash;
+
+public class PlainTextHashService : IHashService
+{
+    public string AlgorithmName => "PlainText";
+
+    public (byte[] hash, byte[] salt) CreateHash(byte[] password)
+    {
+        return CreateHash(password, Array.Empty<byte>());
+    }
+
+    public (byte[] hash, byte[] salt) CreateHash(byte[] password, byte[] salt)
+    {
+        return (password, Array.Empty<byte>());
+    }
+
+    public string CreateHashString(byte[] password)
+    {
+        return Encoding.UTF8.GetString(password);
+    }
+
+    public bool VerifyHash(byte[] password, string hash)
+    {
+        return CryptographicOperations.FixedTimeEquals(password, Encoding.UTF8.GetBytes(hash));
+    }
+}
