c68ad5418d5181137f19e18c3e612f4fbda842c2
Patryk Bartołd <p_bartold@ms.wwsi.edu.pl>
2024-08-22 13:58:25 +0200

    RsaSha256DigitalSignature implementation

---
+++ ComputerShop/Server/Cryptography/DigitalSignature/IDigitalSignature.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace ComputerShop.Server.Cryptography.DigitalSignature
+{
+    public interface IDigitalSignature
+    {
+        SigningCredentials GetSigningCredentials(IConfiguration configuration);
+    }
+}
---
+++ ComputerShop/Server/Cryptography/DigitalSignature/RsaSha256DigitalSignature.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace ComputerShop.Server.Cryptography.DigitalSignature;
+
+public class RsaSha256DigitalSignature : IDigitalSignature
+{
+    public SigningCredentials GetSigningCredentials(IConfiguration configuration)
+    {
+        string rsaPem = configuration["Settings:TokenPrivateRSA"];
+
+        using RSA rsa = RSA.Create();
+        rsa.ImportFromPem(rsaPem);
+
+        return new(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+    }
+}
