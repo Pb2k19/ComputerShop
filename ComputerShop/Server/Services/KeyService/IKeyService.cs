e623d23c0fb7d545611c00b06a04e833e6604ba4
Patryk Bartołd <p_bartold@ms.wwsi.edu.pl>
2024-08-28 20:03:57 +0200

    IKeyService implementation

---
+++ ComputerShop/Server/Services/KeyService/IKeyService.cs
@@ -0,0 +1,10 @@
+﻿namespace ComputerShop.Server.Services.KeyService;
+
+public interface IKeyService : IDisposable
+{
+    string HashAlgorythmName { get; }
+    (byte[] key, byte[] salt) GetDecryptionKey(int keyLength, string str);
+    (byte[] key, byte[] salt) GetDecryptionKey(int keyLength, string str, byte[] salt);
+    (byte[] key, byte[] salt) GetEncryptionKey(int keyLength, string str);
+    (byte[] key, byte[] salt) GetEncryptionKey(int keyLength, string str, byte[] salt);
+}
---
+++ ComputerShop/Server/Services/KeyService/SymetricKeyService.cs
@@ -0,0 +1,56 @@
+﻿using ComputerShop.Server.Cryptography.Hash;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ComputerShop.Server.Services.KeyService;
+
+public class SymetricKeyService : IKeyService
+{
+    private readonly IHashAlgorithm hash;
+
+    private readonly byte[] key;
+
+    public string HashAlgorythmName => hash.AlgorithmName;
+
+    public SymetricKeyService(IConfiguration configuration, IHashAlgorithm hash)
+    {
+        key = Encoding.UTF8.GetBytes(configuration["Settings:SymetricKey"]);
+        this.hash = hash;
+    }
+
+    public (byte[] key, byte[] salt) GetEncryptionKey(int keyLength, string str)
+    {
+        return hash.CreateHash(CreateKeySource(str), keyLength);
+    }
+
+    public (byte[] key, byte[] salt) GetEncryptionKey(int keyLength, string str, byte[] salt)
+    {
+        return hash.CreateHash(CreateKeySource(str), salt, keyLength);
+    }
+
+    public (byte[] key, byte[] salt) GetDecryptionKey(int keyLength, string str)
+    {
+        return GetEncryptionKey(keyLength, str);
+    }
+
+    public (byte[] key, byte[] salt) GetDecryptionKey(int keyLength, string str, byte[] salt)
+    {
+        return GetEncryptionKey(keyLength, str, salt);
+    }
+
+    private byte[] CreateKeySource(string str)
+    {
+        byte[] strBytes = Encoding.UTF8.GetBytes(str);
+        byte[] array = new byte[key.Length + strBytes.Length];
+
+        strBytes.CopyTo(array, 0);
+        key.CopyTo(array, strBytes.Length);
+
+        return array;
+    }
+
+    public void Dispose()
+    {
+        CryptographicOperations.ZeroMemory(key);
+    }
+}
