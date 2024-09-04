364eeddec6c0e850601acb8c40f9a511cf3f9625
Patryk Bartołd <p_bartold@ms.wwsi.edu.pl>
2024-09-04 23:40:41 +0200

    AsymmetricKeyService implementation

---
+++ ComputerShop/Server/Services/KeyService/AsymmetricKeyService.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ComputerShop.Server.Services.KeyService;
+
+public class AsymmetricKeyService : IKeyService
+{
+    private readonly byte[] pubKey, privKey;
+
+    public string HashAlgorythmName => "None";
+
+    public AsymmetricKeyService(IConfiguration configuration)
+    {
+        pubKey = Encoding.UTF8.GetBytes(configuration["Settings:PubKey"]);
+        privKey = Encoding.UTF8.GetBytes(configuration["Settings:PrivKey"]);
+    }
+
+    public AsymmetricKeyService(byte[] pubKey, byte[] privKey)
+    {
+        this.pubKey = pubKey;
+        this.privKey = privKey;
+    }
+
+    public (byte[] key, byte[] salt) GetDecryptionKey(int _, string str)
+    {
+        return (privKey, Array.Empty<byte>());
+    }
+
+    public (byte[] key, byte[] salt) GetDecryptionKey(int _, string str, byte[] salt)
+    {
+        return (privKey, Array.Empty<byte>());
+    }
+
+    public (byte[] key, byte[] salt) GetEncryptionKey(int _, string str)
+    {
+        return (pubKey, Array.Empty<byte>());
+    }
+
+    public (byte[] key, byte[] salt) GetEncryptionKey(int _, string str, byte[] salt)
+    {
+        return (pubKey, Array.Empty<byte>());
+    }
+
+    public void Dispose()
+    {
+        CryptographicOperations.ZeroMemory(pubKey);
+        CryptographicOperations.ZeroMemory(privKey);
+    }
+}
